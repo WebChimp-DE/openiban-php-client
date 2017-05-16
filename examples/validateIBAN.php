a6d941413af6deb2ba481e05cf367a8998d7e1e3
Kai Neuwerth <darkdust29@googlemail.com>
2017-05-16 20:14:46 +0200

    Update validateIBAN.php

--- examples/validateIBAN.php
+++ examples/validateIBAN.php
@@ -6,14 +6,14 @@ use OpenIban\Client;
 $client = new Client();
 echo '<pre>';
 echo "\$client = new Client();" . PHP_EOL;
-echo "\$client->validate() => " . PHP_EOL;
+echo "\$client->validate('DE89370400440532013000') => " . PHP_EOL;
 var_dump($client->validate('DE89370400440532013000'));
 echo PHP_EOL;
 
-echo "\$client->validate(true) => " . PHP_EOL;
+echo "\$client->validate('DE89370400440532013000', true) => " . PHP_EOL;
 var_dump($client->validate('DE89370400440532013000', true));
 echo PHP_EOL;
 
-echo "\$client->validate(true, true) => " . PHP_EOL;
+echo "\$client->validate('DE89370400440532013000', true, true) => " . PHP_EOL;
 var_dump($client->validate('DE89370400440532013000', true, true));
 echo '</pre>';
